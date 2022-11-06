d431177fc4e844966c252d25a6182e09136bddac
jmq19950824 <jiangmq95@163.com>
2022-11-06 17:49:39 +0800

    by Minqi

--- components.py
+++ components.py
@@ -4,6 +4,8 @@ from networks import MLP, AE
 import rtdl
 
 from sklearn.preprocessing import MinMaxScaler, Normalizer
+from torch.utils.data import Subset, DataLoader, TensorDataset
+from utils import Utils
 
 
 # we decouple the network components from the existing literature
@@ -11,39 +13,46 @@ class Components():
     def __init__(self, data,
                  augmentation=None,
                  preprocess=None,
-                 network=None,
-                 strategy=None,
-                 loss=None,
+                 network_name=None,
+                 training_strategy=None,
+                 loss_name=None,
                  optimizer_name=None):
+
+        self.utils = Utils()
 
         # input data
         self.data = data
 
         # global parameters
-        self.augmentation = augmentation
-        self.preprocess = preprocess
-        self.network = network
-        self.strategy = strategy
-        self.loss = loss
-        self.optimizer_name = optimizer_name
+        self.augmentation = augmentation # data augmentation
+        self.preprocess = preprocess # data preprocessing
+        self.network_name = network_name # network architecture name
+        self.training_strategy = training_strategy # training strategy
+        self.loss_name = loss_name # loss function name
+        self.optimizer_name = optimizer_name # optmizer name
+
+        self.batch_resample = True
+        self.epochs = 50
+        self.batch_size = 64
+        self.lr = 1e-3
 
     def gym(self): #
         pool = {}
 
         pool['augmentation'] = ['None']
         pool['preprocess'] = ['minmax', 'normalize']
-        pool['network'] = ['MLP', 'AE', 'ResNet', 'FTT']
-        pool['strategy'] = ['None']
-        pool['loss'] = ['minus', 'inverse', 'hinge', 'deviation']
-        pool['optimizer'] = ['SGD', 'Adam', 'RMSprop']
+        pool['network_name'] = ['MLP', 'AE', 'ResNet', 'FTT']
+        pool['training_strategy'] = ['None']
+        pool['loss_name'] = ['minus', 'inverse', 'hinge', 'deviation']
+        pool['optimizer_name'] = ['SGD', 'Adam', 'RMSprop']
 
         return pool
 
-    def data_augmentation(self):
+    def f_augmentation(self):
         # TODO
         pass
 
-    def data_preprocessing(self):
+    def f_preprocess(self):
         if self.preprocess == 'minmax':
             scaler = MinMaxScaler().fit(self.data['X_train'])
         elif self.preprocess == 'normalize':
@@ -52,7 +61,26 @@ class Components():
         self.data['X_train'] = scaler.transform(self.data['X_train'])
         self.data['X_test'] = scaler.transform(self.data['X_test'])
 
-    def network(self):
+        # train loader
+        if self.batch_resample:
+            X_train_resample, y_train_resample = self.utils.sampler(self.data['X_train'], self.data['y_train'], self.batch_size)
+            train_tensor = TensorDataset(torch.from_numpy(X_train_resample).float(),
+                                         torch.tensor(y_train_resample).float())
+            self.train_loader = DataLoader(train_tensor, batch_size=self.batch_size, shuffle=False, drop_last=True)
+        else:
+            train_tensor = TensorDataset(torch.from_numpy(self.data['X_train']).float(),
+                                         torch.tensor(self.data['y_train']).float())
+            self.train_loader = DataLoader(train_tensor, batch_size=self.batch_size, shuffle=True, drop_last=True)
+
+        # testing tensor
+        self.test_tensor = torch.from_numpy(self.data['X_test']).float()
+
+        return self
+
+
+
+
+    def f_network(self):
         '''
         We including several network architectures, including:
         - MLP
@@ -63,13 +91,13 @@ class Components():
 
         input_size = self.data['X_train'].shape[1]
 
-        if self.network == 'MLP':
+        if self.network_name == 'MLP':
             self.model = MLP(input_size=input_size, act_fun=nn.ReLU())
 
-        elif self.network == 'AE':
+        elif self.network_name == 'AE':
             self.model = AE(input_size=input_size, act_fun=nn.ReLU())
 
-        elif self.network == 'ResNet':
+        elif self.network_name == 'ResNet':
             self.model = rtdl.ResNet.make_baseline(
                         d_in=input_size,
                         d_main=128,
@@ -80,7 +108,7 @@ class Components():
                         d_out=1)
             self.model.add_module('reg', nn.BatchNorm1d(num_features=1))
 
-        elif self.network == 'FTT':
+        elif self.network_name == 'FTT':
             self.model = rtdl.FTTransformer.make_default(
                 n_num_features=input_size,
                 cat_cardinalities=None,
@@ -92,13 +120,13 @@ class Components():
         else:
             raise NotImplementedError
 
-        return self.model
+        return self
 
-    def training_strategy(self):
+    def f_training_strategy(self):
         # TODO
         pass
 
-    def loss(self, s_n, s_a):
+    def f_loss(self, s_n, s_a):
         '''
         We including several loss functions in the existing AD algorithms, including:
         - Minus loss
@@ -109,13 +137,16 @@ class Components():
         '''
         ranking_loss = torch.nn.MarginRankingLoss(margin=5.0) # for hinge loss
 
-        if self.loss == 'minus':
+        if self.loss_name == 'minus':
             loss = torch.mean(s_n + torch.max(torch.zeros_like(s_a), 5.0 - s_a))
-        elif self.loss == 'inverse':
+
+        elif self.loss_name == 'inverse':
             loss = torch.mean(torch.pow(s_n, torch.ones_like(s_n)) + torch.pow(s_a, -1 * torch.ones_like(s_a)))
-        elif self.loss == 'hinge':
+
+        elif self.loss_name == 'hinge':
             loss = ranking_loss(s_a, s_n, torch.ones_like(s_a))
-        elif self.loss == 'deviation':
+
+        elif self.loss_name == 'deviation':
             ref = torch.randn(5000)  # sampling from the normal distribution
             s_n = (s_n - torch.mean(ref)) / torch.std(ref) # normalized anomaly score
             s_a = (s_a - torch.mean(ref)) / torch.std(ref) # normalized anomaly score
@@ -128,29 +159,64 @@ class Components():
 
         return loss
 
-    def optimizer(self):
+    def f_optimizer(self):
         # TODO: weight decay
         if self.optimizer_name == 'SGD':
-        elif self.optimizer_name == 'Adam':
-        elif self.optimizer_name == 'RMSprop':
-            optimizer = torch.optim.RMSprop(self.model.parameters(), lr=self.lr)
+            self.optimizer = torch.optim.SGD(self.model.parameters(), lr=self.lr)
 
+        elif self.optimizer_name == 'Adam':
+            self.optimizer = torch.optim.Adam(self.model.parameters(), lr=self.lr)
+
+        elif self.optimizer_name == 'RMSprop':
+            self.optimizer = torch.optim.RMSprop(self.model.parameters(), lr=self.lr)
+
+        else:
+            raise NotImplementedError
+
+        return self
 
     def train(self):
-        # initialization
-        self.model = network()
+        # data augmentation
+        self.f_augmentation()
+
+        # data preprocessing
+        self.f_preprocess()
+
+        # network initialization
+        self.f_network()
 
         # optimizer
-
-
+        self.f_optimizer()
 
         # fitting
+        for epoch in range(self.epochs):
+            for batch in self.train_loader:
+                # data
+                X, y = batch
 
+                # clear gradient
+                self.model.zero_grad()
 
+                # loss forward
+                _, s = self.model(X)
+                s_n = s[y==0]
+                s_a = s[y==1]
+                loss = self.f_loss(s_n, s_a)
 
+                # loss backward
+                loss.backward()
 
-        return self.model
+                # gradient update
+                self.optimizer.step()
 
+        return self
+
+    @torch.no_grad()
     def predict_score(self):
+        self.model.eval()
+
+        _, score_test = self.model(self.test_tensor)
+        score_test = score_test.numpy()
+        metrics = self.utils.metric(y_true=self.data['y_test'], y_score=score_test, pos_label=1)
 
         return metrics
---
+++ utils.py
@@ -0,0 +1,71 @@
+import numpy as np
+import random
+import torch
+# metric
+from sklearn.metrics import roc_auc_score, average_precision_score
+
+class Utils():
+    def __init__(self):
+        pass
+
+    # remove randomness
+    def set_seed(self, seed):
+        # basic seed
+        np.random.seed(seed)
+        random.seed(seed)
+
+        # pytorch seed
+        torch.manual_seed(seed)
+        torch.backends.cudnn.deterministic = True
+        torch.backends.cudnn.benchmark = False
+
+    def get_device(self, gpu_specific=False):
+        if gpu_specific:
+            if torch.cuda.is_available():
+                n_gpu = torch.cuda.device_count()
+                print(f'number of gpu: {n_gpu}')
+                print(f'cuda name: {torch.cuda.get_device_name(0)}')
+                print('GPU is on')
+            else:
+                print('GPU is off')
+
+            device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+        else:
+            device = torch.device("cpu")
+
+        return device
+
+    # metric
+    def metric(self, y_true, y_score, pos_label=1):
+        aucroc = roc_auc_score(y_true=y_true, y_score=y_score)
+        aucpr = average_precision_score(y_true=y_true, y_score=y_score, pos_label=1)
+
+        return {'aucroc':aucroc, 'aucpr':aucpr}
+
+    # resampling function
+    def sampler(self, X_train, y_train, batch_size):
+        index_u = np.where(y_train == 0)[0]
+        index_a = np.where(y_train == 1)[0]
+
+        n = 0
+        while len(index_u) >= batch_size:
+            self.set_seed(n)
+            index_u_batch = np.random.choice(index_u, batch_size // 2, replace=False)
+            index_u = np.setdiff1d(index_u, index_u_batch)
+
+            index_a_batch = np.random.choice(index_a, batch_size // 2, replace=True)
+
+            # batch index
+            index_batch = np.append(index_u_batch, index_a_batch)
+            # shuffle
+            np.random.shuffle(index_batch)
+
+            if n == 0:
+                X_train_new = X_train[index_batch]
+                y_train_new = y_train[index_batch]
+            else:
+                X_train_new = np.append(X_train_new, X_train[index_batch], axis=0)
+                y_train_new = np.append(y_train_new, y_train[index_batch])
+            n += 1
+
+        return X_train_new, y_train_new
